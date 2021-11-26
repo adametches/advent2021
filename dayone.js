f051012e7068208ba196bce301c0766c4bb7e03c
Adam Etches <adametches@gmail.com>
2021-11-26 22:02:48 +0000

    update

---
+++ dayone.js
@@ -0,0 +1,11 @@
+import {$,jQuery} from 'jquery';
+
+function readdata()
+
+    $.get("https://adventofcode.com/2020/day/1/input", function(data){
+        console.log("Data: " + data);
+    });
+    https://adventofcode.com/2020/day/1/input
+
+
+readdata()
