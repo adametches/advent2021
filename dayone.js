9c9d3ae8b4568f0a3d752964adde541961af683e
Adam Etches <adametches@gmail.com>
2021-12-01 11:45:53 +0000

    part2

--- dayone.js
+++ dayone.js
@@ -1,8 +1,16 @@
-function solution(inputdata) {
-    const dataArray = inputdata.split('\n').map(Number);
+function solutionPart1(inputData) {
+    const dataArray = inputData.split('\n').map(Number);
     const numberIncreased = dataArray.filter((depth, index, array) => {
         return depth > array[index-1];
-        }
-    )
+    })
+    return numberIncreased.length;   
+}
+function solutionPart2(inputData) {
+    const dataArray = inputData.split('\n').map(Number);
+    const numberIncreased = dataArray.map((depth, index, array) => {
+        return depth + array[index+1] + array[index+2];
+        }).filter((depth, index, array) => {
+            return depth > array[index-1];
+    })   
     return numberIncreased.length;   
 }
