bbc546db00978891070abdb7f29a9346f0eed527
Adam Etches <adametches@gmail.com>
2021-12-02 09:07:09 +0000

    add template

---
+++ advent21.etches.uk/_daytemplate.js
@@ -0,0 +1,8 @@
+function solutionPart1(inputData) {
+    const dataArray = inputData.split('\n')
+    return "part1";   
+}
+function solutionPart2(inputData) {
+    const dataArray = inputData.split('\n')
+    return "part2";   
+}
