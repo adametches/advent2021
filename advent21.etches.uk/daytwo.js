554713a9554625abf2461e864e08996ae1bbbd03
Adam Etches <adametches@gmail.com>
2021-12-02 09:48:40 +0000

    part 2 code cleanup

--- advent21.etches.uk/daytwo.js
+++ advent21.etches.uk/daytwo.js
@@ -6,12 +6,10 @@ function solutionPart1(inputData){
         'up': (value) => depth -= value,
         'down': (value) => depth += value,
       };
-
     dataArray.forEach(element => { 
         var [command, value] = element.split(" ");
         value = parseInt(value);
-        moveSub[command](value);
-  
+        moveSub[command](value); 
     });
     return horizontal * depth;
 }
@@ -19,21 +17,15 @@ function solutionPart1(inputData){
 function solutionPart2(inputData){
     const dataArray = inputData.split('\n')
     var horizontal = 0, depth = 0, aim = 0;
+    const moveSub = {
+        'forward': (value) => {horizontal += value; depth += value * aim},
+        'up': (value) => aim -= value,
+        'down': (value) => aim += value,
+    };
     dataArray.forEach(element => {
         var [command, value] = element.split(" ");
         value = parseInt(value);
-        switch(command) {
-            case "forward":
-                horizontal += value;
-                depth += value * aim;
-                break;
-            case "up":
-                aim -= value;
-                break;
-            case "down":
-                aim += value;
-                break;
-          }       
+        moveSub[command](value);
     });
     return horizontal * depth;
 }
