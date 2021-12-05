1beb708adc10cc46ffc333da3c8fb071c2491376
Adam Etches <adametches@gmail.com>
2021-12-05 15:52:05 +0000

    part1

--- advent21.etches.uk/dayfive.js
+++ advent21.etches.uk/dayfive.js
@@ -1,8 +1,7 @@
 function solutionPart1(inputData) {
     const dataArray = inputData.split('\n')
     const lines = []
-    dataArray.forEach(element => {
-     
+    dataArray.forEach(element => {    
         var [start,end] = element.split(" -> ");
         var [x1,y1] = start.split(",");
         var [x2,y2] = end.split(",");
@@ -11,8 +10,7 @@ function solutionPart1(inputData) {
   
     });
 
-    //const grid = {coords: Array.from(Array(1000), () => new Array(1000).fill(0))}
-    const grid = {coords: Array.from(Array(10), () => new Array(10).fill(0))}
+    const grid = {coords: Array.from(Array(1000), () => new Array(1000).fill(0))}
     intersectCount = 0;
     lines.forEach(line => {
         if(line.x1 == line.x2 ){
@@ -20,8 +18,6 @@ function solutionPart1(inputData) {
             else {yMin = line.y2, yMax = line.y1}
             for (let index = yMin; index <= yMax; index++) {              
                 grid.coords[index][line.x1] += 1
-                if (grid.coords[index][line.x1] > 1) intersectCount += 1
-                
             }
 
         }
@@ -29,21 +25,16 @@ function solutionPart1(inputData) {
             if (line.x1 < line.x2) {var xMin = line.x1; xMax = line.x2}
             else {var xMin = line.x2; var xMax = line.x1}
             for (let index = xMin; index <= xMax; index++) {              
-                grid.coords[line.y1][index] += 1
-                if (grid.coords[line.y1][index] > 1) intersectCount += 1
-               
+                grid.coords[line.y1][index] += 1              
             }
-        }
-        
+        }        
     });
-
-    var newCount = 0;
+    var intersectCount = 0;
     for (let i = 0; i < grid.coords.length; i++) { 
         for (let j = 0; j < grid.coords.length; j++) { 
-            if (grid.coords[i][j] > 1) newCount += 1
+            if (grid.coords[i][j] > 1) intersectCount += 1
         }
     }
-
     return intersectCount;   
 }
 function solutionPart2(inputData) {
