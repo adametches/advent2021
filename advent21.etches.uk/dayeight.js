4f136fc6bdd7ca307b47d94ed5d6514c2ebefd4a
adametches <adametches@gmail.com>
2021-12-07 16:08:00 +0000

    dayeigth template

---
+++ advent21.etches.uk/dayeight.js
@@ -0,0 +1,17 @@
+function solutionPart1(inputData) {
+     
+    inputData = `One
+    Two
+    Three`
+
+    const dataArray = inputData.split('\n')
+    
+    return "part1";   
+}
+function solutionPart2(inputData) {
+    inputData = `One
+    Two
+    Three`
+    const dataArray = inputData.split('\n')
+    return "part2";   
+}
