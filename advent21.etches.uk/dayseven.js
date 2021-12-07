b80a67ccb7b20144fa9264cbe5a8d020047937bd
Adam Etches <adametches@gmail.com>
2021-12-07 05:34:54 +0000

    partone

--- advent21.etches.uk/dayseven.js
+++ advent21.etches.uk/dayseven.js
@@ -1,12 +1,24 @@
 function solutionPart1(inputData) {
      
-    inputData = `One
-    Two
-    Three`
+    inputData = "16,1,2,0,4,2,7,1,2,14"
+    const dataArray = inputData.split(',')
+    var solution;
+    var leastFuel ;
 
-    const dataArray = inputData.split('\n')
+    dataArray.forEach(endposition => {
+        fuel = 0;
+        dataArray.forEach(crabposition => {
+            fuel += Math.abs(crabposition - endposition)          
+        });
+        if (leastFuel == undefined || fuel < leastFuel){
+            leastFuel = fuel
+            solution = {endposition:endposition,leastFuel:leastFuel}
+        }
+    });
+    console.log(solution)
+
     
-    return "part1";   
+    return solution.leastFuel;   
 }
 function solutionPart2(inputData) {
     inputData = `One
