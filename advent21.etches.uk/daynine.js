347f95fab3733a815b8ed6f4679a12ca348288d0
Adam Etches <adametches@gmail.com>
2021-12-08 19:25:21 +0000

    start day 9

---
+++ advent21.etches.uk/daynine.js
@@ -0,0 +1,14 @@
+function solutionPart1(inputData) {
+     
+    inputData = `One
+    Two
+    Three`
+
+    const dataArray = inputData.split('\n')
+    
+    return "part1";   
+}
+function solutionPart2(inputData) {
+    const dataArray = inputData.split('\n')
+    return "part2";   
+}
