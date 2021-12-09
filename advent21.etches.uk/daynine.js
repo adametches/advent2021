f20b796e24440823903df4a34a48c1109090540d
Adam Etches <adametches@gmail.com>
2021-12-09 06:33:28 +0000

    part2 start

--- advent21.etches.uk/daynine.js
+++ advent21.etches.uk/daynine.js
@@ -1,40 +1,52 @@
 function solutionPart1(inputData) {
-     
+    /*    
+       inputData = `2199943210
+   3987894921
+   9856789892
+   8767896789
+   9899965678`
+   */
+    const dataArray = inputData.split('\n').map(x => x.split('').map(Number))
+    const riskLevel = []
+    for (i = 0; i < dataArray.length; i++) {
+        const row = dataArray[i];
+        for (j = 0; j < row.length; j++) {
+            const point = row[j];
+            const adjacent = findAdjacent(dataArray, row, i, j)
+            if (adjacent.filter(x => x <= point).length === 0) riskLevel.push(point + 1)
+        }
+    }
+    return riskLevel.reduce((a, b) => a + b, 0);
+}
+function solutionPart2(inputData) {
+    /*    
     inputData = `2199943210
 3987894921
 9856789892
 8767896789
 9899965678`
-
+*/
     const dataArray = inputData.split('\n').map(x => x.split('').map(Number))
-    const riskLevel =  []
-    for (i= 0; i < dataArray.length; i++) {
+    const riskLevel = []
+    for (i = 0; i < dataArray.length; i++) {
         const row = dataArray[i];
-        for (j= 0; j < row.length; j++) {
-            console.log(i,",",j)
+        for (j = 0; j < row.length; j++) {
             const point = row[j];
-            let adjacent = [];
-            if (dataArray[i-1] == undefined){
-                adjacent = [row[j-1],row[j+1],dataArray[i+1][j]]
-            }
-            else if (dataArray[i+1] == undefined){
-                adjacent = [row[j-1],row[j+1],dataArray[i-1][j]]
-            }
-            else{
-                adjacent = [row[j-1],row[j+1],dataArray[i+1][j],dataArray[i-1][j]]
-            }
-            if (adjacent.filter(x=>x<=point).length ===0){
-                console.log("lowpoint = ",i,",",j)
-                riskLevel.push(point +1)
-            }
-           
+            const adjacent = findAdjacent(dataArray, row, i, j)
+            if (adjacent.filter(x => x <= point).length === 0) riskLevel.push(point + 1)
         }
-        
     }
-    
-    return riskLevel.reduce((a, b) => a + b, 0);   
+    return riskLevel.reduce((a, b) => a + b, 0);
 }
-function solutionPart2(inputData) {
-    const dataArray = inputData.split('\n')
-    return "part2";   
+function findAdjacent(outerArray, innerArray, i, j) {
+    if (outerArray[i - 1] == undefined) {
+        return [innerArray[j - 1], innerArray[j + 1], outerArray[i + 1][j]]
+    }
+    else if (outerArray[i + 1] == undefined) {
+        return [innerArray[j - 1], innerArray[j + 1], outerArray[i - 1][j]]
+    }
+    else {
+        return [innerArray[j - 1], innerArray[j + 1], outerArray[i + 1][j], outerArray[i - 1][j]]
+    }
+
 }
