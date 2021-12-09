6cabf0e84b632b52af1e92bbe7b3005baa313bd7
Adam Etches <adametches@gmail.com>
2021-12-09 05:06:51 +0000

    day nine data

--- advent21.etches.uk/daynine.js
+++ advent21.etches.uk/daynine.js
@@ -1,8 +1,10 @@
 function solutionPart1(inputData) {
      
-    inputData = `One
-    Two
-    Three`
+    inputData = `2199943210
+    3987894921
+    9856789892
+    8767896789
+    9899965678`
 
     const dataArray = inputData.split('\n')
     
