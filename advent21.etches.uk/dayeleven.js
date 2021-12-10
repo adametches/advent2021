26dfcf66c7731c9cb7409efea983eb95c13694b5
Adam Etches <adametches@gmail.com>
2021-12-10 20:00:15 +0000

    day eleven

---
+++ advent21.etches.uk/dayeleven.js
@@ -0,0 +1,20 @@
+function solutionPart1(inputData) {
+     
+    inputData = `One
+    Two
+    Three`
+
+    const dataArray = inputData.split('\n')
+    
+    return "part1";   
+}
+function solutionPart2(inputData) {
+    inputData = `One
+    Two
+    Three`
+    const dataArray = inputData.split('\n')
+    return "part2";   
+}
+
+console.log(solutionPart1());
+console.log(solutionPart2());
