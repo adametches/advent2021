2bc76ad344bfb42aab1b5fb1a208dd38ddb16c49
Adam Etches <adametches@gmail.com>
2021-12-15 15:26:50 +0000

    part 2

--- advent21.etches.uk/dayfourteen.js
+++ advent21.etches.uk/dayfourteen.js
@@ -142,7 +142,6 @@ OS -> O`
         letters.push({ [letter]: letter, length: polymerTemplate.filter(x => x === letter).length })
     }
     letters.sort((a, b) => b.length - a.length)
-    console.log(letters)
     let val = letters[0].length - letters[letters.length - 1].length
     return val;
 }
@@ -166,37 +165,151 @@ BB -> N
 BC -> B
 CC -> N
 CN -> C`
+
+inputData = `SHHBNFBCKNHCNOSHHVFF
+
+CK -> N
+VP -> B
+CF -> S
+FO -> V
+VC -> S
+BV -> V
+NP -> P
+SN -> C
+KN -> V
+NF -> P
+SB -> C
+PC -> B
+OB -> V
+NS -> O
+FH -> S
+NK -> S
+HO -> V
+NV -> O
+FV -> O
+FB -> S
+PS -> S
+FN -> K
+HS -> O
+CB -> K
+HV -> P
+NH -> C
+BO -> B
+FF -> N
+PO -> F
+BB -> N
+PN -> C
+BP -> C
+HN -> K
+CO -> P
+BF -> H
+BC -> S
+CV -> B
+VV -> F
+FS -> B
+BN -> P
+VK -> S
+PV -> V
+PP -> B
+PH -> N
+SS -> O
+SK -> S
+NC -> P
+ON -> F
+NB -> N
+CC -> N
+SF -> H
+PF -> H
+OV -> O
+KH -> C
+CP -> V
+PK -> O
+KC -> K
+KK -> C
+KF -> B
+HP -> C
+FK -> H
+BH -> K
+VN -> H
+OO -> S
+SC -> K
+SP -> B
+KO -> V
+KV -> F
+HK -> N
+FP -> N
+NN -> B
+VS -> O
+HC -> K
+BK -> N
+KS -> K
+VB -> O
+OH -> F
+KB -> F
+KP -> H
+HB -> N
+NO -> N
+OF -> O
+BS -> H
+VO -> H
+SH -> O
+SV -> K
+HF -> C
+CS -> F
+FC -> N
+VH -> H
+OP -> K
+OK -> H
+PB -> K
+HH -> S
+OC -> V
+VF -> B
+CH -> K
+CN -> C
+SO -> P
+OS -> O`
     const dataArray = inputData.split('\n')
     let polymerTemplate = dataArray[0].split('')
-    const rules = new Map(dataArray.slice(2).map(x => x.split(" -> ")))
-    const pairCounts = {};
+    const ruleMap = new Map();
+    for (const rule of dataArray.slice(2).map(x => x.split(" -> "))) {
+        ruleMap.set(rule[0],[rule[0][0] + rule[1] ,rule[1] + rule[0][1]] )
+        
+    }
+    let pairCounts = new Map();
     for (let i = 0; i < polymerTemplate.length - 1; i++) {
-        pair = polymerTemplate[i] + polymerTemplate[i + 1]
-        pairCounts[pair]=1
-
+        const pair = polymerTemplate[i] + polymerTemplate[i+1]
+        addToMap(pairCounts,pair)
     }
-    console.log(pairCounts)
-    for (let step = 1; step < 3; step++) {
-        for (const [key, value] of Object.entries(pairCounts)) {
-  
-            newP = rules.get(key)
-            if (pairCounts[key.charAt(0)+newP] == undefined){
-                pairCounts[key.charAt(0)+newP]=1
-            }
-            else{
-            pairCounts[key.charAt(0)+newP]+=1}
+    const lastChar = polymerTemplate[polymerTemplate.length-1]  
+    for (let step = 0; step < 40; step++) {
+        let current = new Map()
+        const keys = pairCounts.keys()
+        for (const key of keys) {
+            const next = ruleMap.get(key)
+            addToMap(current, next[0], pairCounts.get(key))
+            addToMap(current, next[1], pairCounts.get(key))
         }
-    
+        pairCounts = current;
     }
-    console.log('NBCCNBBBCBHCB'.split('').sort().toString())
-    let count = []
-    for (const [key, value] of Object.entries(pairCounts)) {
-        let valueCount
+    const elementCount = new Map();
+    addToMap(elementCount, lastChar)
+    const keys = pairCounts.keys()
+    for (const key of keys) {
+        addToMap(elementCount, key[0],pairCounts.get(key))      
     }
-    
-    return "part2";
+    const values = [...elementCount.values()]
+    const min = Math.min(...values)
+    const max = Math.max(...values)
+    return max-min;
+}
+
+function addToMap(map, pair, val = 1) {
+    if (!map.has(pair)){
+        map.set(pair,0);
+    }
+    map.set(pair,map.get(pair)+val)
 }
 
 
-//console.log(`Part 1: ${solutionPart1()}`);
+console.log(`Part 1: ${solutionPart1()}`);
 console.log(`Part 2: ${solutionPart2()}`);
