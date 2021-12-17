9737b581590c18e2e546c0c6545c90ff03b1ede2
Adam Etches <adametches@gmail.com>
2021-12-17 16:19:12 +0000

    alternative part2

--- advent21.etches.uk/dayfifteen.js
+++ advent21.etches.uk/dayfifteen.js
@@ -255,8 +255,8 @@ function solutionPart2(inputData) {
 
     }
 
+    let result = mapPart2(caveMap);
 
-    let result = minPathSum(caveMap) - caveMap[0][0];
 
     return result;
 }
@@ -279,6 +279,60 @@ function minPathSum(grid) {
     return grid[m - 1][n - 1];
 };
 
+function minPathSum2(map){
+
+    let lowestTotalRisk = Math.min(
+        Array.from({ length: map.length }).reduce((total, _, i) => total + map[0][i] + map[i][map.length - 1], 0),
+        Array.from({ length: map.length }).reduce((total, _, i) => total + map[i][0] + map[map.length - 1][i], 0)
+      );
+    
+      const distances = Array
+        .from({ length: map.length })
+        .map(() => Array
+          .from({ length: map.length })
+          .map(() => Infinity));
+    
+      const walk = (cost, [px, py]) => {
+        if (cost >= distances[py][px]) {
+          return;
+        }
+    
+        if (px === map.length - 1 && py === map.length - 1) {
+          if (cost < lowestTotalRisk) {
+            lowestTotalRisk = cost;
+          }
+        }
+    
+        distances[py][px] = cost;
+    
+        const neighbors = [
+          [px + 1, py], // right
+          [px - 1, py], // left
+          [px, py + 1], // down
+          [px, py - 1], // up
+        ];
+    
+        for (const [nx, ny] of neighbors) {
+          // out of bounds
+          if (nx < 0 || nx >= map.length || ny < 0 || ny >= map.length) {
+            continue;
+          }
+    
+          // cost too high
+          if (cost + map[ny][nx] >= lowestTotalRisk) {
+            continue;
+          }
+    
+          walk(cost + map[ny][nx], [nx, ny]);
+        }
+      };
+    
+      walk(0, [0, 0]);
+    
+      return lowestTotalRisk;
+
+
+}
 
 function print2dArray(array) {
     array.forEach(rwo => {
