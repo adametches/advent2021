6b6b7be993b61c0e9f2b32e9680bdfd06dc605eb
Adam Etches <adametches@gmail.com>
2021-12-17 22:45:23 +0000

    midway

--- advent21.etches.uk/daysixteen.js
+++ advent21.etches.uk/daysixteen.js
@@ -1,42 +1,72 @@
 function solutionPart1(inputData) {
-     
-    inputData = `0123456789
-0123456789
-0123456789
-0123456789
-0123456789
-0123456789
-0123456789
-0123456789
-0123456789`
 
-
-    const dataArray = inputData.split('\n').map(row => row.split(''))
-    print2dArray(dataArray)
-    
-    return "part1";   
+    inputData = `38006F45291200`
+    const binaryNumber = inputData.split('').map(d => parseInt(d, 16).toString(2).padStart(4, '0')).join('')
+    decodePacket(binaryNumber,[])
+    console.log(JSON.stringify(packet))
+    return "part1";
 }
 function solutionPart2(inputData) {
     inputData = `One
 Two
 Three`
     const dataArray = inputData.split('\n')
-    return "part2";   
+    return "part2";
 }
 
-function print2dArray(array) {
-    array.forEach(rwo => {
-        let printrwo = '['
-        rwo.forEach(cell => {
 
-            printrwo += cell + ']['
-        });
-        printrwo = printrwo.substring(0, printrwo.length - 1);
-        console.log(printrwo)
-        
-    });
+function decodePacket(packetText, packetArray) {
+    console.log(packetText)
+    const packetVersion = parseInt(packetText.substring(0, 3), 2)
+    const packetType = parseInt(packetText.substring(3, 6), 2)
+    const packetValue = ''
+    let packetContent = packetText.substring(6)
+    if (packetType == 4) {
+        let binaryString = ''
+        while (packetContent.length > 0) {
+
+            binaryString += packetContent.substring(1, 5)
+
+            if (packetContent[0] == '0') {
+
+                packetContent = ''
+            }
+            packetContent = packetContent.substring(5)
+
+        }
+        packetValue = parseInt(binaryString, 2);
+    }
+    else{
+        // operater package
+        console.log('packet content',packetContent)
+        const lengthTypeId = packetContent[0];
+        console.log('lengthTypeId',lengthTypeId)
+        if (lengthTypeId  === '0'){
+            binaryString = packetContent.substring(1, 16)
+            console.log('binaryString', binaryString)
+            let length = parseInt(binaryString, 2) 
+            console.log(length)
+            
+
+
+
+        }
+        else{
+            binaryString = packetContent.substring(1, 12)
+            console.log(binaryString)
+            let length = parseInt(binaryString, 2) 
+            console.log(length)
+        }
+    }
+
+    return {
+        packetVersion: packetVersion,
+        packetType: packetType,
+        packetValue: packetValue
+    }
+
 
 }
 
 console.log(`Part 1: ${solutionPart1()}`);
-console.log(`Part 2: ${solutionPart2()}`);
+//console.log(`Part 2: ${solutionPart2()}`);
