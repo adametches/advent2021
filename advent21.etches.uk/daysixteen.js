1ee7354d3e63081199e783f29c7598662989509d
Adam Etches <adametches@gmail.com>
2021-12-18 11:17:56 +0000

    easy examples done

--- advent21.etches.uk/daysixteen.js
+++ advent21.etches.uk/daysixteen.js
@@ -1,10 +1,12 @@
 function solutionPart1(inputData) {
 
     inputData = `38006F45291200`
+    inputData = 'EE00D40C823060'
+    inputData = '8A004A801A8002F478'
     const binaryNumber = inputData.split('').map(d => parseInt(d, 16).toString(2).padStart(4, '0')).join('')
-    decodePacket(binaryNumber,[])
-    console.log(JSON.stringify(packet))
-    return "part1";
+    packet = decodePacket(binaryNumber,[])
+    console.log(packet)
+    return packet.sumOfVersions();
 }
 function solutionPart2(inputData) {
     inputData = `One
@@ -16,10 +18,9 @@ Three`
 
 
 function decodePacket(packetText, packetArray) {
-    console.log(packetText)
+    console.log('decodePacket', packetText)
     const packetVersion = parseInt(packetText.substring(0, 3), 2)
     const packetType = parseInt(packetText.substring(3, 6), 2)
-    const packetValue = ''
     let packetContent = packetText.substring(6)
     if (packetType == 4) {
         let binaryString = ''
@@ -34,36 +35,70 @@ function decodePacket(packetText, packetArray) {
             packetContent = packetContent.substring(5)
 
         }
-        packetValue = parseInt(binaryString, 2);
+        const packetValue = parseInt(binaryString, 2);
+
+        return new Packet(packetVersion,packetType,packetValue)
+
+       
+    
     }
     else{
         // operater package
         console.log('packet content',packetContent)
         const lengthTypeId = packetContent[0];
         console.log('lengthTypeId',lengthTypeId)
+        const packet =  new Packet(packetVersion,packetType,0)
         if (lengthTypeId  === '0'){
-            binaryString = packetContent.substring(1, 16)
-            console.log('binaryString', binaryString)
-            let length = parseInt(binaryString, 2) 
-            console.log(length)
+            lengthInBits = packetContent.substring(1, 16)
+            let length = parseInt(lengthInBits, 2) 
+            packetContent = packetContent.substring(16)
+            console.log('packet content',packetContent)
             
-
+            console.log(length)
+            packet.packetArray.push(decodePacket(packetContent.substring(0,11)))
+            packetContent = packetContent.substring(11)
+            packet.packetArray.push(decodePacket(packetContent.substring(0,16)))
+            packetContent = packetContent.substring(16)
 
 
         }
         else{
-            binaryString = packetContent.substring(1, 12)
-            console.log(binaryString)
-            let length = parseInt(binaryString, 2) 
+            lengthInBits = packetContent.substring(1, 12)
+            packetContent = packetContent.substring(12)
+            console.log('packet content',packetContent)
+            let length = parseInt(lengthInBits, 2) 
             console.log(length)
+            while(length > 0){
+                packet.packetArray.push(decodePacket(packetContent.substring(0, 11)))
+                packetContent = packetContent.substring(11)
+                length--;
+
+            }
         }
+        return packet;
     }
 
-    return {
-        packetVersion: packetVersion,
-        packetType: packetType,
-        packetValue: packetValue
+   
+}
+class Packet{
+
+    constructor(packetVersion,packetType,packetValue){
+        this.packetVersion = packetVersion;
+        this.packetType = packetType;
+        this.packetValue = packetValue;
+        this.packetArray = []
     }
+
+    sumOfVersions() {
+        let sum = this.packetVersion
+        this.packetArray.forEach(subpacket => {
+            sum = sum + subpacket.packetVersion
+        });
+
+        return sum;
+        
+    }
+
 
 
 }
